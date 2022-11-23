ae793a31758b1465fa4ed063a6512fa3119ca719
RainerTeixeira <raineroliveira94@hotmail.com>
2022-11-23 16:22:04 -0300

    Desestruturando array

--- Very Easy/1-AjudandoaAcademia.js
+++ Very Easy/1-AjudandoaAcademia.js
@@ -1,11 +1,11 @@
-function media(numeros) {
+function media(...numeros) {
     const soma = numeros.reduce((accum, num) => accum + num, 0)
 
     return soma / numeros.length
   }
 
-console.log(media([10, 9, 6, 8, 9, 1, 5, 7]))    //6.875
-console.log(media([2, 5, 7, 1, -2]))             //2.6
-console.log(media([10, 10, 10, 10, 9]))          //9.8
-console.log(media([25, 75,]))                    //50
+console.log(media(10, 9, 6, 8, 9, 1, 5, 7))    //6.875
+console.log(media(2, 5, 7, 1, -2))             //2.6
+console.log(media(10, 10, 10, 10, 9))          //9.8
+console.log(media(25, 75,))                    //50
  
