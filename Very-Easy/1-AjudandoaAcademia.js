d772d218d3f09472ea4785da5bacdc166f1256c1
RainerTeixeira <raineroliveira94@hotmail.com>
2022-11-23 16:45:07 -0300

    Desestruturando array

--- Very-Easy/1-AjudandoaAcademia.js
+++ Very-Easy/1-AjudandoaAcademia.js
@@ -4,7 +4,7 @@ function media(...numeros) {
     return soma / numeros.length
   }
 
-console.log(media(10, 9, 6, 8, 9, 1, 5, 7))    //6.875
+console.log(media(10, 9, 6, 8, 9, 1, 4, 8))    //6.875
 console.log(media(2, 5, 7, 1, -2))             //2.6
 console.log(media(10, 10, 10, 10, 9))          //9.8
 console.log(media(25, 75,))                    //50
