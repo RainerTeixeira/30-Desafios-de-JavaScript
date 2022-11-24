43804f5d6fc343a18c0e72d788d337361360faa3
RainerTeixeira <raineroliveira94@hotmail.com>
2022-11-23 21:42:48 -0300

    História Escondida I

--- Very-Easy/5-HistoriaEscondida1.js
+++ Very-Easy/5-HistoriaEscondida1.js
@@ -9,3 +9,4 @@ console.log(letraMaisAlta('lorem impsum dolorec sec avanti'))   // v
 console.log(letraMaisAlta('Hello'))                             // o 
 console.log(letraMaisAlta('may the force be with your'))       // y
 console.log(letraMaisAlta('it s over nine thousand'))      // v 
+console.log(letraMaisAlta('Rainer'))      // r 
