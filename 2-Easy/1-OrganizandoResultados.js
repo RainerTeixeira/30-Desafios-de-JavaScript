e732ce3eb83f29d50b5210ea995d8ce0e9c2cdc6
RainerTeixeira <raineroliveira94@hotmail.com>
2022-11-29 12:30:04 -0300

    Organizando Resultados

--- 2-Easy/1-OrganizandoResultados.js
+++ 2-Easy/1-OrganizandoResultados.js
@@ -1,10 +1,15 @@
+function nivelarordenar(arr) {
+    const numero = []
+
+    arr.forEach(list => {
+       numero.push(...list) 
+    });
+
+    return numero.sort((a, b) => a -b )
+}
 
 
-
-
-
-
-console.log(ordenar([ [1,5,3], [6,19,11], [47,128,5], [1,93,57,42,103] ]))
-console.log(ordenar([ [1,3], [4, 8], [7,5], [2,6] ]))
-console.log(ordenar([ [], [], [], [], [] ]))
-console.log(ordenar([ [100, 50], [60, 100], [20,100, 70], [10, 40, 80,] ]))
+console.log(nivelarordenar([ [1,5,3], [6,19,11], [47,128,5], [1,93,57,42,103] ]))
+console.log(nivelarordenar([ [1,3], [4, 8], [7,5], [2,6] ]))
+console.log(nivelarordenar([ [], [], [], [], [] ]))
+console.log(nivelarordenar([ [100, 50], [60, 100], [20,100, 70], [10, 40, 80,] ]))
