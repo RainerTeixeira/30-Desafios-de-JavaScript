ed81c25034d6ab5bfd7ffb4bd9bfd3ea1cfee676
RainerTeixeira <raineroliveira94@hotmail.com>
2022-11-29 13:16:14 -0300

    Biblioteca Interplanetária

--- 2-Easy/2-BibliotecaInterplanetaria.js
+++ 2-Easy/2-BibliotecaInterplanetaria.js
@@ -1,13 +1,15 @@
 function ordenarstring(nomeCompleto) {  const nomes = nomeCompleto.split(' ')
-  let initials = ''
+  let inicial = ''
 
   for (let i = 0; i < nomes.length - 1; i++) {
-    initials += ` ${nomes[i][0].toUpperCase()}.`;
+    inicial += ` ${nomes[i][0].toUpperCase()}.`;
   }
-  return `${nomes[nomes.length - 1].toUpperCase()},${initials}`
+  return `${nomes[nomes.length - 1].toUpperCase()},${inicial}`
 }
 
 console.log(ordenarstring('isac Larrubia Ferreira Pontes'))
 console.log(ordenarstring('Jonh Ronaldo Reuel Tolkien'))
 console.log(ordenarstring('Chistoper James Paolini'))
 console.log(ordenarstring('Suzane Marie Collins'))
+console.log(ordenarstring('Rainer Oliveira Teixeira'))
+
