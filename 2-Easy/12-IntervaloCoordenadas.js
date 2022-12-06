51e3fd63df98c9328289dd3643b9e9a50cec0198
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-06 16:59:31 -0300

    Intervalo de Coordenadas

---
+++ 2-Easy/12-IntervaloCoordenadas.js
@@ -0,0 +1,16 @@
+function parMenor(par) {
+    const result = []
+
+    for (let i = 0; i <= par.length[0]; i++) {
+        for (let j = 0; j <= par[1]; j++) {
+            result.push([i, j])
+        }
+
+    }
+    return result
+}
+
+console.log(parMenor([2, 2]))
+console.log(parMenor([2, 7]))
+console.log(parMenor([-3, -3]))
+console.log(parMenor([7, 6]))
