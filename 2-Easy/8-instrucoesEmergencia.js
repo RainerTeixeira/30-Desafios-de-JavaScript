a1e46b78e01e6da6c4e9aee7fa1ef8df6e309853
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-06 15:04:16 -0300

    Instruções de Emergência

--- 2-Easy/8-instrucoesEmergencia.js
+++ 2-Easy/8-instrucoesEmergencia.js
@@ -1,14 +1,14 @@
 function verificarLetra(str) {
-    const  contagemCaracter = {}
+    const contagemCaracter = {}
 
-    for (let i = 0; i< str.length; i++) {
-      contagemCaracter[str[i]] = contagemCaracter[str[i]] ? contagemCaracter[str[i]] + 1 : 1        
+    for (let i = 0; i < str.length; i++) {
+        contagemCaracter[str[i]] = contagemCaracter[str[i]] ? contagemCaracter[str[i]] + 1 : 1
+
     }
-    return contagemCaracter
+    return Object.values(contagemCaracter).every((count, index, array) => index !== 0 ? count === array[index - 1] : true)
 }
 
 console.log(verificarLetra('this is thee')) // verdadeiro
 console.log(verificarLetra('ssd')) // falso
 console.log(verificarLetra('lorem ipsum')) // falso
 console.log(verificarLetra('QQwweerrttyy')) // verdadeiro
-
