f25ad023dc6bac7562972d6a20baa01dad94ebc9
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-07 12:47:53 -0300

    Código Romano II

--- 3-Medium/16-codigoRomano2.js
+++ 3-Medium/16-codigoRomano2.js
@@ -1,6 +1,23 @@
+function decifrar(str, chave) {
+    const caracteresArray = str.split('')
+    const AjustaChave = chave % 26
 
+    const codesArray = caracteresArray.map(char => {
+        const CodigoAtual = char.charCodeAt(0)
 
+        if (CodigoAtual - AjustaChave < 65 && CodigoAtual >= 65 && CodigoAtual <= 90) {
+            return CodigoAtual + 26
+        }
 
+        if (CodigoAtual - AjustaChave < 97 && CodigoAtual >= 97 && CodigoAtual <= 122) {
+            return CodigoAtual + 26
+        }
+
+        return CodigoAtual
+    })
+
+    return codesArray.map(code => String.fromCharCode(code - AjustaChave)).join('')
+}
 
 console.log(decifrar('Vguvg', 2))
 console.log(decifrar('BCDYZABcdyza', 27))
