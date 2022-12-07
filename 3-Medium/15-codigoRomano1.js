dca8edbfc21a86dccb0cb9a5f566ebaf192101d6
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-06 21:53:56 -0300

    Código Romano I

--- 3-Medium/15-codigoRomano1.js
+++ 3-Medium/15-codigoRomano1.js
@@ -1,36 +1,52 @@
-function romanNumeral(str) {  const numeralsArray = str.split('')
-
-  const decimalsArray = numeralsArray.map(numeral => {
-    switch (numeral) {
-      case 'I':
-        return 1
-      case 'V':
-        return 5
-      case 'X':
-        return 10
-      case 'L':
-        return 50
-      case 'C':
-        return 100
-      case 'D':
-        return 500
-      case 'M':
-        return 1000
-      default:
-        return 0
-    }
-  })
-
-  return decimalsArray.reduceRight((accum, atual, index, array) => {
-    if (atual < array[index + 1]) {
-      return accum - atual
-    } else {
-      return accum + atual
-    }
-  }, 0)
+function somaValor(accum, atual, index, array) {
+  return atual < array[index + 1] ? accum - atual : accum + atual
 }
 
-console.log(romanNumeral('XLVII'))     //47
-console.log(romanNumeral('CDXXXVIII')) //438
-console.log(romanNumeral('CMIX'))      //909
-console.log(romanNumeral('MMMCMXCIX')) //3999
+function numeralRomano(str) {
+  const numeralsArray = str.split('')
+
+  const numeralRomanoMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  }
+
+  const decimalsArray = numeralsArray.map(numeral => numeralRomanoMap[numeral])
+
+  return decimalsArray.reduceRight(somaValor)
+}
+
+console.log(numeralRomano('XLVII'))     //47
+console.log(numeralRomano('CDXXXVIII')) //438
+console.log(numeralRomano('CMIX'))      //909
+console.log(numeralRomano('MMMCMXCIX')) //3999
+function somaValor(accum, atual, index, array) {
+  return atual < array[index + 1] ? accum - atual : accum + atual
+}
+
+function numeralRomano(str) {
+  const numeralsArray = str.split('')
+
+  const numeralRomanoMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  }
+
+  const decimalsArray = numeralsArray.map(numeral => numeralRomanoMap[numeral])
+
+  return decimalsArray.reduceRight(somaValor)
+}
+
+console.log(numeralRomano('XLVII'))     //47
+console.log(numeralRomano('CDXXXVIII')) //438
+console.log(numeralRomano('CMIX'))      //909
+console.log(numeralRomano('MMMCMXCIX')) //3999
