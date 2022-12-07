6ba684261729e0bcd942edadc180ee4852947c19
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-06 21:30:14 -0300

    Código Romano I

--- 3-Medium/15-codigoRomano1.js
+++ 3-Medium/15-codigoRomano1.js
@@ -1,4 +1,36 @@
+function romanNumeral(str) {  const numeralsArray = str.split('')
 
+  const decimalsArray = numeralsArray.map(numeral => {
+    switch (numeral) {
+      case 'I':
+        return 1
+      case 'V':
+        return 5
+      case 'X':
+        return 10
+      case 'L':
+        return 50
+      case 'C':
+        return 100
+      case 'D':
+        return 500
+      case 'M':
+        return 1000
+      default:
+        return 0
+    }
+  })
 
+  return decimalsArray.reduceRight((accum, atual, index, array) => {
+    if (atual < array[index + 1]) {
+      return accum - atual
+    } else {
+      return accum + atual
+    }
+  }, 0)
+}
 
-console.log()
+console.log(romanNumeral('XLVII'))     //47
+console.log(romanNumeral('CDXXXVIII')) //438
+console.log(romanNumeral('CMIX'))      //909
+console.log(romanNumeral('MMMCMXCIX')) //3999
