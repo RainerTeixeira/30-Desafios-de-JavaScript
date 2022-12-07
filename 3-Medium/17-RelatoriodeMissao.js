787c654c243b0f60e2c816d02da7338b761ac4f9
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-07 13:25:26 -0300

    Relatório de Missão

--- 3-Medium/17-RelatoriodeMissao.js
+++ 3-Medium/17-RelatoriodeMissao.js
@@ -1,14 +1,15 @@
-function inserirNaString(str, paraInserir, indexes) {  const charsArray = str.split('')
-  let inserirContador = 0
+function inserirNaString(str, paraInserir, indexes) {
+    const charsArray = str.split('')
+    let inserirContador = 0
 
-  for (let i = 0; i <= str.length; i++) {
-    if (indexes.includes(i)) {
-      charsArray.splice(i + inserirContador, 0, paraInserir)
-      inserirContador++
+    for (let i = 0; i <= str.length; i++) {
+        if (indexes.includes(i)) {
+            charsArray.splice(i + inserirContador, 0, paraInserir)
+            inserirContador++
+        }
     }
-  }
 
-  return charsArray.join('')
+    return charsArray.join('')
 }
 
 console.log(inserirNaString('Capaz utilizar as capsulas emergencia', 'de', [0, 6, 27, 38]))
