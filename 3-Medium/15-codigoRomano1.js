04d3b0a57e19d4af85f14d9ba83f16fd4878270a
RainerTeixeira <raineroliveira94@hotmail.com>
2022-12-06 21:55:53 -0300

    Código Romano I

--- 3-Medium/15-codigoRomano1.js
+++ 3-Medium/15-codigoRomano1.js
@@ -1,29 +1,4 @@
-function somaValor(accum, atual, index, array) {
-  return atual < array[index + 1] ? accum - atual : accum + atual
-}
 
-function numeralRomano(str) {
-  const numeralsArray = str.split('')
-
-  const numeralRomanoMap = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-  }
-
-  const decimalsArray = numeralsArray.map(numeral => numeralRomanoMap[numeral])
-
-  return decimalsArray.reduceRight(somaValor)
-}
-
-console.log(numeralRomano('XLVII'))     //47
-console.log(numeralRomano('CDXXXVIII')) //438
-console.log(numeralRomano('CMIX'))      //909
-console.log(numeralRomano('MMMCMXCIX')) //3999
 function somaValor(accum, atual, index, array) {
   return atual < array[index + 1] ? accum - atual : accum + atual
 }
